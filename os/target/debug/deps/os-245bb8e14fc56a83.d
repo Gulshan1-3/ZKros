9bdb44eb6c5bdf3d8f3fe310a7b0a83beaa76482
Gulshan1-3 <ankitstar955@gmail.com>
2024-12-23 10:05:53 +0530

    start

---
+++ os/target/debug/deps/os-245bb8e14fc56a83.d
@@ -0,0 +1,5 @@
+/home/gulshansharma/ZKros/os/target/debug/deps/libos-245bb8e14fc56a83.rmeta: src/main.rs
+
+/home/gulshansharma/ZKros/os/target/debug/deps/os-245bb8e14fc56a83.d: src/main.rs
+
+src/main.rs:
---
+++ os/target/debug/deps/os-60ba1d41bce045b0.d
@@ -0,0 +1,5 @@
+/home/gulshansharma/ZKros/os/target/debug/deps/libos-60ba1d41bce045b0.rmeta: src/main.rs
+
+/home/gulshansharma/ZKros/os/target/debug/deps/os-60ba1d41bce045b0.d: src/main.rs
+
+src/main.rs:
